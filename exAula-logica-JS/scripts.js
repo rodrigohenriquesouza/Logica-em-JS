8d2920b4ea7679757a420f45e63d8a4a631f8498
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-09-01 11:46:46 -0300

    primeiros exercicios de logica para treino

---
+++ exAula-logica-JS/scripts.js
@@ -0,0 +1,5 @@
+var nome = prompt('Qual é o seu nome? ')
+alert(`Bem-vindo, ${nome}`)
+
+var endereco = 1.2 //numero decimal / numero real
+var estarGravida = false //boolean onde o dado é true ou false
