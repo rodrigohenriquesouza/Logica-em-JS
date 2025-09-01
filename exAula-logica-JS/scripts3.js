0de9c4f6d8f160d906253c3befd6d4e7a4e5ac44
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-09-01 13:34:37 -0300

    treinamento logica

---
+++ exAula-logica-JS/scripts3.js
@@ -0,0 +1,7 @@
+// Criar um programa para o Usuário digitar dois números e calcular a média entre eles
+
+var n1 = Number(prompt('Digite um número'))
+var n2 = Number(prompt('Digite outro número'))
+
+let media = (n1 + n2) / 2
+alert(`A soma entre o número ${n1} e o número ${n2} tem a media ${media}`)
