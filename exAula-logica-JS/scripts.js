38369c684d6ea74a3118cd1623df719eabcc0f7f
Rodrigo Henrique Souza <rodrigohsouza84@gmail.com>
2025-09-01 11:52:57 -0300

    organizando pastas dos exercicios

--- exAula-logica-JS/scripts.js
+++ exAula-logica-JS/scripts.js
@@ -1,5 +1,2 @@
-var nome = prompt('Qual é o seu nome? ')
-alert(`Bem-vindo, ${nome}`)
-
-var endereco = 1.2 //numero decimal / numero real
-var estarGravida = false //boolean onde o dado é true ou false
+var nome = prompt('Digite seu nome ')
+alert(`Boas-vindas, ${nome}`)
